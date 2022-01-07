9b3c1b929731bf9eabf854b63de60ddccdea9b0a
cracked-machine <chrisjsutton@gmail.com>
2022-01-07 23:01:23 +0000

    added switch presets and tx/rx functions

--- inc/adg2188.hpp
+++ inc/adg2188.hpp
@@ -52,11 +52,115 @@ public:
     // @brief prevent copy assignment of the I2C handle pointer
     Driver &operator=(Driver const &other) = delete;
 
+    enum class XLineRead
+    {
+        X0      = 0x34,     //  00110100
+        X1      = 0x3C,     //  00111100
+        X2      = 0x74,     //  01110100
+        X3      = 0x3E,     //  01111100
+        X4      = 0x35,     //  00110101
+        X5      = 0x3D,     //  00111101
+        X6      = 0x75,     //  01110101
+        X7      = 0x7D      //  01111101
+    };
+
+    enum class Latch
+    {
+        // @brief set the latch after this switch instruction 
+        set,
+        // @brief do *not* set the latch after this instruction 
+        reset
+    };
+
+    enum class Throw
+    {
+        open        = 0x00,
+        close       = 0x80,        
+    };
+
+    enum class Pole
+    {
+        x0_to_y0    = 0x00,
+        x0_to_y1    = 0x01,
+        x0_to_y2    = 0x02,
+        x0_to_y3    = 0x03,
+        x0_to_y4    = 0x04,
+        x0_to_y5    = 0x05,
+        x0_to_y6    = 0x06,
+        x0_to_y7    = 0x07,
+
+        x1_to_y0    = 0x08,
+        x1_to_y1    = 0x09,
+        x1_to_y2    = 0x0A,
+        x1_to_y3    = 0x0B,
+        x1_to_y4    = 0x0C,
+        x1_to_y5    = 0x0D,
+        x1_to_y6    = 0x0E,
+        x1_to_y7    = 0x0F,
+
+        x2_to_y0    = 0x10,
+        x2_to_y1    = 0x11,
+        x2_to_y2    = 0x12,
+        x2_to_y3    = 0x13,
+        x2_to_y4    = 0x14,
+        x2_to_y5    = 0x15,
+        x2_to_y6    = 0x16,
+        x2_to_y7    = 0x17,        
+
+        x3_to_y0    = 0x18,
+        x3_to_y1    = 0x19,
+        x3_to_y2    = 0x1A,
+        x3_to_y3    = 0x1B,
+        x3_to_y4    = 0x1C,
+        x3_to_y5    = 0x1D,
+        x3_to_y6    = 0x1E,
+        x3_to_y7    = 0x1F,
+
+        x4_to_y0    = 0x20,
+        x4_to_y1    = 0x21,
+        x4_to_y2    = 0x22,
+        x4_to_y3    = 0x23,
+        x4_to_y4    = 0x24,
+        x4_to_y5    = 0x25,
+        x4_to_y6    = 0x26,
+        x4_to_y7    = 0x27,
+
+        x5_to_y0    = 0x28,
+        x5_to_y1    = 0x29,
+        x5_to_y2    = 0x2A,
+        x5_to_y3    = 0x2B,
+        x5_to_y4    = 0x2C,
+        x5_to_y5    = 0x2D,
+        x5_to_y6    = 0x2E,
+        x5_to_y7    = 0x2F,
+
+        x6_to_y0    = 0x30,
+        x6_to_y1    = 0x31,
+        x6_to_y2    = 0x32,
+        x6_to_y3    = 0x33,
+        x6_to_y4    = 0x34,
+        x6_to_y5    = 0x35,
+        x6_to_y6    = 0x36,
+        x6_to_y7    = 0x37,
+
+        x7_to_y0    = 0x38,
+        x7_to_y1    = 0x39,
+        x7_to_y2    = 0x3A,
+        x7_to_y3    = 0x3B,
+        x7_to_y4    = 0x3C,
+        x7_to_y5    = 0x3D,
+        x7_to_y6    = 0x3E,
+        x7_to_y7    = 0x3F,
+    };
+
+
     bool probe_i2c();
 
-    bool async_set_one_switch();
-    bool sync_set_all_switches();
-    bool read_xline_switch_values();
+    bool write_switch(const Throw &sw_throw, const Pole &sw_pole, const Latch &sw_latch);
+    //bool sync_set_all_switches();
+    bool read_xline_switch_values(XLineRead line);
+
+
 
 private:
 
--- src/adg2188.cpp
+++ src/adg2188.cpp
@@ -30,6 +30,32 @@ Driver::Driver()
 {
     _i2c_handle = std::unique_ptr<I2C_TypeDef>(I2C2);
     probe_i2c();
+
+    // close/open asynchronously, i.e. one at a time
+    read_xline_switch_values(XLineRead::X2);
+    write_switch(Throw::close, Pole::x2_to_y0, Latch::set);
+    read_xline_switch_values(XLineRead::X2);
+    read_xline_switch_values(XLineRead::X1);
+    write_switch(Throw::close, Pole::x1_to_y0, Latch::set);
+    read_xline_switch_values(XLineRead::X1);
+    read_xline_switch_values(XLineRead::X2);
+    write_switch(Throw::open, Pole::x2_to_y0, Latch::set);
+    read_xline_switch_values(XLineRead::X2);
+    read_xline_switch_values(XLineRead::X1);
+    write_switch(Throw::open, Pole::x1_to_y0, Latch::set);
+    read_xline_switch_values(XLineRead::X1);
+
+    // close/open synchronously, i.e. at the same time
+    read_xline_switch_values(XLineRead::X1);
+    read_xline_switch_values(XLineRead::X2);
+    write_switch(Throw::close, Pole::x2_to_y0, Latch::reset);
+    write_switch(Throw::close, Pole::x1_to_y0, Latch::set);
+    
+    write_switch(Throw::open, Pole::x2_to_y0, Latch::reset);
+    write_switch(Throw::open, Pole::x1_to_y0, Latch::set);
+    read_xline_switch_values(XLineRead::X1);
+    read_xline_switch_values(XLineRead::X2);    
+
 }
 
 bool Driver::probe_i2c()
@@ -45,16 +71,78 @@ bool Driver::probe_i2c()
 
 }
 
-// bool Driver::read_xline_switch_values()
-// {
-//     bool success {true};
+// See page 20 of https://www.analog.com/media/en/technical-documentation/data-sheets/adg2188.pdf
+bool Driver::write_switch(const Throw &sw_throw, const Pole &sw_pole, const Latch &sw_latch)
+{
+    // read this number of bytes
+	LL_I2C_SetTransferSize(_i2c_handle.get(), 2);
 
-//     // check ADG2188 is listening on 0xE0.
-// 	if (stm32::i2c::send_addr(_i2c_handle.get(), i2c_addr, stm32::i2c::MsgType::PROBE) == stm32::i2c::Status::NACK) 
-//     {
-//         success = false;
-//     }
-//     return success;
-// }
+    bool success {true};
+
+    // check ADG2188 is listening on 0xE0 + 1.
+	if (stm32::i2c::send_addr(_i2c_handle, i2c_addr, stm32::i2c::MsgType::WRITE) == stm32::i2c::Status::NACK) 
+    {
+        success = false;
+    }    
+
+    // switch config byte
+    uint8_t switch_configuration = (static_cast<uint8_t>(sw_throw) | static_cast<uint8_t>(sw_pole));
+    stm32::i2c::send_data(_i2c_handle, switch_configuration);
+    // latch byte
+    if (sw_latch == Latch::set)
+    {
+        stm32::i2c::send_data(_i2c_handle, 0x01);
+    }
+    else
+    {
+        stm32::i2c::send_data(_i2c_handle, 0x00);
+    }
+    
+
+    LL_I2C_GenerateStopCondition(_i2c_handle.get());
+
+    return success;
+
+
+}
+
+// See page 22 of https://www.analog.com/media/en/technical-documentation/data-sheets/adg2188.pdf
+bool Driver::read_xline_switch_values(XLineRead line)
+{
+    // read this number of bytes
+	LL_I2C_SetTransferSize(_i2c_handle.get(), 2);
+
+    bool success {true};
+
+    // check ADG2188 is listening on 0xE0 + 1.
+	if (stm32::i2c::send_addr(_i2c_handle, i2c_addr, stm32::i2c::MsgType::WRITE) == stm32::i2c::Status::NACK) 
+    {
+        success = false;
+    }
+    // request the xline we want to read (second byte is don't care, so just repeat it)
+    stm32::i2c::send_command(_i2c_handle, static_cast<uint8_t>(line));
+    stm32::i2c::send_command(_i2c_handle, static_cast<uint8_t>(line));
+
+	LL_I2C_GenerateStopCondition(_i2c_handle.get());
+
+    // check ADG2188 is listening on 0xE0 + 0.
+	if (stm32::i2c::send_addr(_i2c_handle, i2c_addr, stm32::i2c::MsgType::READ) == stm32::i2c::Status::NACK) 
+    {
+        success = false;
+    }    
+
+    // receive the first byte and send back ACk to slave
+    uint8_t rx_byte1 {0};
+    stm32::i2c::receive_data(_i2c_handle, rx_byte1);
+    LL_I2C_AcknowledgeNextData(_i2c_handle.get(), LL_I2C_ACK);
+
+    // receive the second byte, send NACK and STOP to slave
+    uint8_t rx_byte2 {0};
+    stm32::i2c::receive_data(_i2c_handle, rx_byte2);
+    LL_I2C_AcknowledgeNextData(_i2c_handle.get(), LL_I2C_NACK);
+    LL_I2C_GenerateStopCondition(_i2c_handle.get());
+
+    return success;
+}
 
 } // namespace adg2188
