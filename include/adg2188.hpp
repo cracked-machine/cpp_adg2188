6418d45e8f3d5c1077119103886eca6f866120c8
cracked-machine <chrisjsutton@gmail.com>
2022-07-27 12:47:06 +0100

    updated to use references

--- include/adg2188.hpp
+++ include/adg2188.hpp
@@ -22,153 +22,149 @@
 
 #ifndef __ADG2188_HPP__
 #define __ADG2188_HPP__
-    
-#include <i2c_utils.hpp>
+
+#include <i2c_utils_ref.hpp>
 
 // disable dynamic allocation/copying
 #include <restricted_base.hpp>
 
-
 namespace adg2188
 {
 
-// @brief Class to control the ADG2188 CrossPoint "Analog Switch Array" 
+// @brief Class to control the ADG2188 CrossPoint "Analog Switch Array"
 class Driver : public RestrictedBase
 {
 public:
-    explicit Driver(I2C_TypeDef *i2c_handle);
-    
-    // @brief prevent copy construction of the I2C handle pointer
-    Driver(Driver const &other) = delete;
-    // @brief prevent copy assignment of the I2C handle pointer
-    Driver &operator=(Driver const &other) = delete;
+  explicit Driver(I2C_TypeDef *i2c_handle);
 
-    enum class XLineRead
-    {
-        X0      = 0x34,     //  00110100
-        X1      = 0x3C,     //  00111100
-        X2      = 0x74,     //  01110100
-        X3      = 0x3E,     //  01111100
-        X4      = 0x35,     //  00110101
-        X5      = 0x3D,     //  00111101
-        X6      = 0x75,     //  01110101
-        X7      = 0x7D      //  01111101
-    };
+  // @brief prevent copy construction of the I2C handle pointer
+  Driver(Driver const &other) = delete;
+  // @brief prevent copy assignment of the I2C handle pointer
+  Driver &operator=(Driver const &other) = delete;
 
-    enum class Latch
-    {
-        // @brief set the latch after this switch instruction 
-        set,
-        // @brief do *not* set the latch after this instruction 
-        reset
-    };
+  enum class XLineRead
+  {
+    X0 = 0x34, //  00110100
+    X1 = 0x3C, //  00111100
+    X2 = 0x74, //  01110100
+    X3 = 0x3E, //  01111100
+    X4 = 0x35, //  00110101
+    X5 = 0x3D, //  00111101
+    X6 = 0x75, //  01110101
+    X7 = 0x7D  //  01111101
+  };
 
-    enum class Throw
-    {
-        open        = 0x00,
-        close       = 0x80,        
-    };
+  enum class Latch
+  {
+    // @brief set the latch after this switch instruction
+    set,
+    // @brief do *not* set the latch after this instruction
+    reset
+  };
 
-    enum class Pole
-    {
-        x0_to_y0    = 0x00,
-        x0_to_y1    = 0x01,
-        x0_to_y2    = 0x02,
-        x0_to_y3    = 0x03,
-        x0_to_y4    = 0x04,
-        x0_to_y5    = 0x05,
-        x0_to_y6    = 0x06,
-        x0_to_y7    = 0x07,
+  enum class Throw
+  {
+    open  = 0x00,
+    close = 0x80,
+  };
 
-        x1_to_y0    = 0x08,
-        x1_to_y1    = 0x09,
-        x1_to_y2    = 0x0A,
-        x1_to_y3    = 0x0B,
-        x1_to_y4    = 0x0C,
-        x1_to_y5    = 0x0D,
-        x1_to_y6    = 0x0E,
-        x1_to_y7    = 0x0F,
+  enum class Pole
+  {
+    x0_to_y0 = 0x00,
+    x0_to_y1 = 0x01,
+    x0_to_y2 = 0x02,
+    x0_to_y3 = 0x03,
+    x0_to_y4 = 0x04,
+    x0_to_y5 = 0x05,
+    x0_to_y6 = 0x06,
+    x0_to_y7 = 0x07,
 
-        x2_to_y0    = 0x10,
-        x2_to_y1    = 0x11,
-        x2_to_y2    = 0x12,
-        x2_to_y3    = 0x13,
-        x2_to_y4    = 0x14,
-        x2_to_y5    = 0x15,
-        x2_to_y6    = 0x16,
-        x2_to_y7    = 0x17,        
+    x1_to_y0 = 0x08,
+    x1_to_y1 = 0x09,
+    x1_to_y2 = 0x0A,
+    x1_to_y3 = 0x0B,
+    x1_to_y4 = 0x0C,
+    x1_to_y5 = 0x0D,
+    x1_to_y6 = 0x0E,
+    x1_to_y7 = 0x0F,
 
-        x3_to_y0    = 0x18,
-        x3_to_y1    = 0x19,
-        x3_to_y2    = 0x1A,
-        x3_to_y3    = 0x1B,
-        x3_to_y4    = 0x1C,
-        x3_to_y5    = 0x1D,
-        x3_to_y6    = 0x1E,
-        x3_to_y7    = 0x1F,
+    x2_to_y0 = 0x10,
+    x2_to_y1 = 0x11,
+    x2_to_y2 = 0x12,
+    x2_to_y3 = 0x13,
+    x2_to_y4 = 0x14,
+    x2_to_y5 = 0x15,
+    x2_to_y6 = 0x16,
+    x2_to_y7 = 0x17,
 
-        x4_to_y0    = 0x20,
-        x4_to_y1    = 0x21,
-        x4_to_y2    = 0x22,
-        x4_to_y3    = 0x23,
-        x4_to_y4    = 0x24,
-        x4_to_y5    = 0x25,
-        x4_to_y6    = 0x26,
-        x4_to_y7    = 0x27,
+    x3_to_y0 = 0x18,
+    x3_to_y1 = 0x19,
+    x3_to_y2 = 0x1A,
+    x3_to_y3 = 0x1B,
+    x3_to_y4 = 0x1C,
+    x3_to_y5 = 0x1D,
+    x3_to_y6 = 0x1E,
+    x3_to_y7 = 0x1F,
 
-        x5_to_y0    = 0x28,
-        x5_to_y1    = 0x29,
-        x5_to_y2    = 0x2A,
-        x5_to_y3    = 0x2B,
-        x5_to_y4    = 0x2C,
-        x5_to_y5    = 0x2D,
-        x5_to_y6    = 0x2E,
-        x5_to_y7    = 0x2F,
+    x4_to_y0 = 0x20,
+    x4_to_y1 = 0x21,
+    x4_to_y2 = 0x22,
+    x4_to_y3 = 0x23,
+    x4_to_y4 = 0x24,
+    x4_to_y5 = 0x25,
+    x4_to_y6 = 0x26,
+    x4_to_y7 = 0x27,
 
-        x6_to_y0    = 0x40,
-        x6_to_y1    = 0x41,
-        x6_to_y2    = 0x42,
-        x6_to_y3    = 0x43,
-        x6_to_y4    = 0x44,
-        x6_to_y5    = 0x45,
-        x6_to_y6    = 0x46,
-        x6_to_y7    = 0x47,
+    x5_to_y0 = 0x28,
+    x5_to_y1 = 0x29,
+    x5_to_y2 = 0x2A,
+    x5_to_y3 = 0x2B,
+    x5_to_y4 = 0x2C,
+    x5_to_y5 = 0x2D,
+    x5_to_y6 = 0x2E,
+    x5_to_y7 = 0x2F,
 
-        x7_to_y0    = 0x48,
-        x7_to_y1    = 0x49,
-        x7_to_y2    = 0x4A,
-        x7_to_y3    = 0x4B,
-        x7_to_y4    = 0x4C,
-        x7_to_y5    = 0x4D,
-        x7_to_y6    = 0x4E,
-        x7_to_y7    = 0x4F,
-    };
+    x6_to_y0 = 0x40,
+    x6_to_y1 = 0x41,
+    x6_to_y2 = 0x42,
+    x6_to_y3 = 0x43,
+    x6_to_y4 = 0x44,
+    x6_to_y5 = 0x45,
+    x6_to_y6 = 0x46,
+    x6_to_y7 = 0x47,
 
-    bool probe_i2c();
+    x7_to_y0 = 0x48,
+    x7_to_y1 = 0x49,
+    x7_to_y2 = 0x4A,
+    x7_to_y3 = 0x4B,
+    x7_to_y4 = 0x4C,
+    x7_to_y5 = 0x4D,
+    x7_to_y6 = 0x4E,
+    x7_to_y7 = 0x4F,
+  };
 
-    // @brief Tell the crosspoint IC to open or close a switch pole
-    // @param sw_throw Throw::open or Throw::close
-    // @param sw_pole The x and y pole configuration to throw
-    // @param sw_latch Set the switch now or not
-    // @return Always true
-    bool write_switch(const Throw &sw_throw, const Pole &sw_pole, const Latch &sw_latch);
+  bool probe_i2c();
 
-    // @brief Opens all switch poles.
-    // @return Always true
-    bool clear_all();
+  // @brief Tell the crosspoint IC to open or close a switch pole
+  // @param sw_throw Throw::open or Throw::close
+  // @param sw_pole The x and y pole configuration to throw
+  // @param sw_latch Set the switch now or not
+  // @return Always true
+  bool write_switch(const Throw &sw_throw, const Pole &sw_pole, const Latch &sw_latch);
 
-    // @brief Read the register. Useful for debuggin.
-    // @param line The all the X poles from the switch
-    // @return always True
-    bool read_xline_switch_values(XLineRead line);
+  // @brief Opens all switch poles.
+  // @return Always true
+  bool clear_all();
 
-
+  // @brief Read the register. Useful for debuggin.
+  // @param line The all the X poles from the switch
+  // @return always True
+  bool read_xline_switch_values(XLineRead line);
 
 private:
+  uint8_t i2c_addr{0xE0};
 
-    uint8_t i2c_addr { 0xE0 };
-    // std::unique_ptr<I2C_TypeDef> m_i2c_handle;
-    I2C_TypeDef* m_i2c_handle;
+  I2C_TypeDef &m_i2c_handle;
 };
 
 } // namespace adg2188
--- src/adg2188.cpp
+++ src/adg2188.cpp
@@ -22,202 +22,200 @@
 
 #include <adg2188.hpp>
 
-
 namespace adg2188
 {
 
 Driver::Driver(I2C_TypeDef *i2c_handle)
+    : m_i2c_handle(*i2c_handle)
 {
-    // m_i2c_handle = std::unique_ptr<I2C_TypeDef>(i2c_handle);
-    m_i2c_handle = i2c_handle;
-    probe_i2c();
-    clear_all();
 
-    // // close/open asynchronously, i.e. one at a time
-    // read_xline_switch_values(XLineRead::X2);
-    // write_switch(Throw::close, Pole::x2_to_y0, Latch::set);
-    // read_xline_switch_values(XLineRead::X2);
-    // read_xline_switch_values(XLineRead::X1);
-    // write_switch(Throw::close, Pole::x1_to_y0, Latch::set);
-    // read_xline_switch_values(XLineRead::X1);
-    // read_xline_switch_values(XLineRead::X2);
-    // write_switch(Throw::open, Pole::x2_to_y0, Latch::set);
-    // read_xline_switch_values(XLineRead::X2);
-    // read_xline_switch_values(XLineRead::X1);
-    // write_switch(Throw::open, Pole::x1_to_y0, Latch::set);
-    // read_xline_switch_values(XLineRead::X1);
+  probe_i2c();
+  clear_all();
 
-    // // close/open synchronously, i.e. at the same time
-    // read_xline_switch_values(XLineRead::X1);
-    // read_xline_switch_values(XLineRead::X2);
-    // write_switch(Throw::close, Pole::x2_to_y0, Latch::reset);
-    // write_switch(Throw::close, Pole::x1_to_y0, Latch::set);
-    
-    // write_switch(Throw::open, Pole::x2_to_y0, Latch::reset);
-    // write_switch(Throw::open, Pole::x1_to_y0, Latch::set);
-    // read_xline_switch_values(XLineRead::X1);
-    // read_xline_switch_values(XLineRead::X2);    
+  // // close/open asynchronously, i.e. one at a time
+  // read_xline_switch_values(XLineRead::X2);
+  // write_switch(Throw::close, Pole::x2_to_y0, Latch::set);
+  // read_xline_switch_values(XLineRead::X2);
+  // read_xline_switch_values(XLineRead::X1);
+  // write_switch(Throw::close, Pole::x1_to_y0, Latch::set);
+  // read_xline_switch_values(XLineRead::X1);
+  // read_xline_switch_values(XLineRead::X2);
+  // write_switch(Throw::open, Pole::x2_to_y0, Latch::set);
+  // read_xline_switch_values(XLineRead::X2);
+  // read_xline_switch_values(XLineRead::X1);
+  // write_switch(Throw::open, Pole::x1_to_y0, Latch::set);
+  // read_xline_switch_values(XLineRead::X1);
 
+  // // close/open synchronously, i.e. at the same time
+  // read_xline_switch_values(XLineRead::X1);
+  // read_xline_switch_values(XLineRead::X2);
+  // write_switch(Throw::close, Pole::x2_to_y0, Latch::reset);
+  // write_switch(Throw::close, Pole::x1_to_y0, Latch::set);
+
+  // write_switch(Throw::open, Pole::x2_to_y0, Latch::reset);
+  // write_switch(Throw::open, Pole::x1_to_y0, Latch::set);
+  // read_xline_switch_values(XLineRead::X1);
+  // read_xline_switch_values(XLineRead::X2);
 }
 
 bool Driver::clear_all()
 {
-    write_switch(Throw::open, Pole::x0_to_y0, Latch::set);
-    write_switch(Throw::open, Pole::x1_to_y0, Latch::set);
-    write_switch(Throw::open, Pole::x2_to_y0, Latch::set);
-    write_switch(Throw::open, Pole::x3_to_y0, Latch::set);
-    write_switch(Throw::open, Pole::x4_to_y0, Latch::set);
-    write_switch(Throw::open, Pole::x5_to_y0, Latch::set);
-    write_switch(Throw::open, Pole::x6_to_y0, Latch::set);
-    write_switch(Throw::open, Pole::x7_to_y0, Latch::set);
+  write_switch(Throw::open, Pole::x0_to_y0, Latch::set);
+  write_switch(Throw::open, Pole::x1_to_y0, Latch::set);
+  write_switch(Throw::open, Pole::x2_to_y0, Latch::set);
+  write_switch(Throw::open, Pole::x3_to_y0, Latch::set);
+  write_switch(Throw::open, Pole::x4_to_y0, Latch::set);
+  write_switch(Throw::open, Pole::x5_to_y0, Latch::set);
+  write_switch(Throw::open, Pole::x6_to_y0, Latch::set);
+  write_switch(Throw::open, Pole::x7_to_y0, Latch::set);
 
-    write_switch(Throw::open, Pole::x0_to_y1, Latch::set);
-    write_switch(Throw::open, Pole::x1_to_y1, Latch::set);
-    write_switch(Throw::open, Pole::x2_to_y1, Latch::set);
-    write_switch(Throw::open, Pole::x3_to_y1, Latch::set);
-    write_switch(Throw::open, Pole::x4_to_y1, Latch::set);
-    write_switch(Throw::open, Pole::x5_to_y1, Latch::set);
-    write_switch(Throw::open, Pole::x6_to_y1, Latch::set);
-    write_switch(Throw::open, Pole::x7_to_y1, Latch::set);
+  write_switch(Throw::open, Pole::x0_to_y1, Latch::set);
+  write_switch(Throw::open, Pole::x1_to_y1, Latch::set);
+  write_switch(Throw::open, Pole::x2_to_y1, Latch::set);
+  write_switch(Throw::open, Pole::x3_to_y1, Latch::set);
+  write_switch(Throw::open, Pole::x4_to_y1, Latch::set);
+  write_switch(Throw::open, Pole::x5_to_y1, Latch::set);
+  write_switch(Throw::open, Pole::x6_to_y1, Latch::set);
+  write_switch(Throw::open, Pole::x7_to_y1, Latch::set);
 
-    write_switch(Throw::open, Pole::x0_to_y2, Latch::set);
-    write_switch(Throw::open, Pole::x1_to_y2, Latch::set);
-    write_switch(Throw::open, Pole::x2_to_y2, Latch::set);
-    write_switch(Throw::open, Pole::x3_to_y2, Latch::set);
-    write_switch(Throw::open, Pole::x4_to_y2, Latch::set);
-    write_switch(Throw::open, Pole::x5_to_y2, Latch::set);
-    write_switch(Throw::open, Pole::x6_to_y2, Latch::set);
-    write_switch(Throw::open, Pole::x7_to_y2, Latch::set);
+  write_switch(Throw::open, Pole::x0_to_y2, Latch::set);
+  write_switch(Throw::open, Pole::x1_to_y2, Latch::set);
+  write_switch(Throw::open, Pole::x2_to_y2, Latch::set);
+  write_switch(Throw::open, Pole::x3_to_y2, Latch::set);
+  write_switch(Throw::open, Pole::x4_to_y2, Latch::set);
+  write_switch(Throw::open, Pole::x5_to_y2, Latch::set);
+  write_switch(Throw::open, Pole::x6_to_y2, Latch::set);
+  write_switch(Throw::open, Pole::x7_to_y2, Latch::set);
 
-    write_switch(Throw::open, Pole::x0_to_y3, Latch::set);
-    write_switch(Throw::open, Pole::x1_to_y3, Latch::set);
-    write_switch(Throw::open, Pole::x2_to_y3, Latch::set);
-    write_switch(Throw::open, Pole::x3_to_y3, Latch::set);
-    write_switch(Throw::open, Pole::x4_to_y3, Latch::set);
-    write_switch(Throw::open, Pole::x5_to_y3, Latch::set);
-    write_switch(Throw::open, Pole::x6_to_y3, Latch::set);
-    write_switch(Throw::open, Pole::x7_to_y3, Latch::set);
+  write_switch(Throw::open, Pole::x0_to_y3, Latch::set);
+  write_switch(Throw::open, Pole::x1_to_y3, Latch::set);
+  write_switch(Throw::open, Pole::x2_to_y3, Latch::set);
+  write_switch(Throw::open, Pole::x3_to_y3, Latch::set);
+  write_switch(Throw::open, Pole::x4_to_y3, Latch::set);
+  write_switch(Throw::open, Pole::x5_to_y3, Latch::set);
+  write_switch(Throw::open, Pole::x6_to_y3, Latch::set);
+  write_switch(Throw::open, Pole::x7_to_y3, Latch::set);
 
-    write_switch(Throw::open, Pole::x0_to_y4, Latch::set);
-    write_switch(Throw::open, Pole::x1_to_y4, Latch::set);
-    write_switch(Throw::open, Pole::x2_to_y4, Latch::set);
-    write_switch(Throw::open, Pole::x3_to_y4, Latch::set);
-    write_switch(Throw::open, Pole::x4_to_y4, Latch::set);
-    write_switch(Throw::open, Pole::x5_to_y4, Latch::set);
-    write_switch(Throw::open, Pole::x6_to_y4, Latch::set);
-    write_switch(Throw::open, Pole::x7_to_y4, Latch::set);
+  write_switch(Throw::open, Pole::x0_to_y4, Latch::set);
+  write_switch(Throw::open, Pole::x1_to_y4, Latch::set);
+  write_switch(Throw::open, Pole::x2_to_y4, Latch::set);
+  write_switch(Throw::open, Pole::x3_to_y4, Latch::set);
+  write_switch(Throw::open, Pole::x4_to_y4, Latch::set);
+  write_switch(Throw::open, Pole::x5_to_y4, Latch::set);
+  write_switch(Throw::open, Pole::x6_to_y4, Latch::set);
+  write_switch(Throw::open, Pole::x7_to_y4, Latch::set);
 
-    write_switch(Throw::open, Pole::x0_to_y5, Latch::set);
-    write_switch(Throw::open, Pole::x1_to_y5, Latch::set);
-    write_switch(Throw::open, Pole::x2_to_y5, Latch::set);
-    write_switch(Throw::open, Pole::x3_to_y5, Latch::set);
-    write_switch(Throw::open, Pole::x4_to_y5, Latch::set);
-    write_switch(Throw::open, Pole::x5_to_y5, Latch::set);
-    write_switch(Throw::open, Pole::x6_to_y5, Latch::set);
-    write_switch(Throw::open, Pole::x7_to_y5, Latch::set);
+  write_switch(Throw::open, Pole::x0_to_y5, Latch::set);
+  write_switch(Throw::open, Pole::x1_to_y5, Latch::set);
+  write_switch(Throw::open, Pole::x2_to_y5, Latch::set);
+  write_switch(Throw::open, Pole::x3_to_y5, Latch::set);
+  write_switch(Throw::open, Pole::x4_to_y5, Latch::set);
+  write_switch(Throw::open, Pole::x5_to_y5, Latch::set);
+  write_switch(Throw::open, Pole::x6_to_y5, Latch::set);
+  write_switch(Throw::open, Pole::x7_to_y5, Latch::set);
 
-    write_switch(Throw::open, Pole::x0_to_y6, Latch::set);
-    write_switch(Throw::open, Pole::x1_to_y6, Latch::set);
-    write_switch(Throw::open, Pole::x2_to_y6, Latch::set);
-    write_switch(Throw::open, Pole::x3_to_y6, Latch::set);
-    write_switch(Throw::open, Pole::x4_to_y6, Latch::set);
-    write_switch(Throw::open, Pole::x5_to_y6, Latch::set);
-    write_switch(Throw::open, Pole::x6_to_y6, Latch::set);
-    write_switch(Throw::open, Pole::x7_to_y6, Latch::set);
+  write_switch(Throw::open, Pole::x0_to_y6, Latch::set);
+  write_switch(Throw::open, Pole::x1_to_y6, Latch::set);
+  write_switch(Throw::open, Pole::x2_to_y6, Latch::set);
+  write_switch(Throw::open, Pole::x3_to_y6, Latch::set);
+  write_switch(Throw::open, Pole::x4_to_y6, Latch::set);
+  write_switch(Throw::open, Pole::x5_to_y6, Latch::set);
+  write_switch(Throw::open, Pole::x6_to_y6, Latch::set);
+  write_switch(Throw::open, Pole::x7_to_y6, Latch::set);
 
-    write_switch(Throw::open, Pole::x0_to_y7, Latch::set);
-    write_switch(Throw::open, Pole::x1_to_y7, Latch::set);
-    write_switch(Throw::open, Pole::x2_to_y7, Latch::set);
-    write_switch(Throw::open, Pole::x3_to_y7, Latch::set);
-    write_switch(Throw::open, Pole::x4_to_y7, Latch::set);
-    write_switch(Throw::open, Pole::x5_to_y7, Latch::set);
-    write_switch(Throw::open, Pole::x6_to_y7, Latch::set);
-    write_switch(Throw::open, Pole::x7_to_y7, Latch::set);                        
-    return true;
+  write_switch(Throw::open, Pole::x0_to_y7, Latch::set);
+  write_switch(Throw::open, Pole::x1_to_y7, Latch::set);
+  write_switch(Throw::open, Pole::x2_to_y7, Latch::set);
+  write_switch(Throw::open, Pole::x3_to_y7, Latch::set);
+  write_switch(Throw::open, Pole::x4_to_y7, Latch::set);
+  write_switch(Throw::open, Pole::x5_to_y7, Latch::set);
+  write_switch(Throw::open, Pole::x6_to_y7, Latch::set);
+  write_switch(Throw::open, Pole::x7_to_y7, Latch::set);
+  return true;
 }
 
 bool Driver::probe_i2c()
 {
-	bool success {true};
+  bool success{true};
 
-    // check ADG2188 is listening on 0xE0.
-	if (stm32::i2c::initialise_slave_device(m_i2c_handle, i2c_addr, stm32::i2c::StartType::PROBE) == stm32::i2c::Status::NACK) 
-    {
-        success = false;
-    }
-    return success;
+  // check ADG2188 is listening on 0xE0.
+  if (stm32::i2c_ref::initialise_slave_device(m_i2c_handle, i2c_addr, stm32::i2c_ref::StartType::PROBE) == stm32::i2c_ref::Status::NACK)
+  {
+    success = false;
+  }
+  return success;
 }
 
 // See page 20 of https://www.analog.com/media/en/technical-documentation/data-sheets/adg2188.pdf
 bool Driver::write_switch(const Throw &sw_throw [[maybe_unused]], const Pole &sw_pole [[maybe_unused]], const Latch &sw_latch [[maybe_unused]])
 {
-    bool success {true};
+  bool success{true};
 
-    // write this number of bytes: The data byte(s) AND the address byte
-    stm32::i2c::set_numbytes(m_i2c_handle, 2);
+  // write this number of bytes: The data byte(s) AND the address byte
+  stm32::i2c_ref::set_numbytes(m_i2c_handle, 2);
 
-    // check ADG2188 is listening on 0xE0 + 1.
-	if (stm32::i2c::initialise_slave_device(m_i2c_handle, i2c_addr, stm32::i2c::StartType::WRITE) == stm32::i2c::Status::NACK) 
-    {
-        success = false;
-    }    
+  // check ADG2188 is listening on 0xE0 + 1.
+  if (stm32::i2c_ref::initialise_slave_device(m_i2c_handle, i2c_addr, stm32::i2c_ref::StartType::WRITE) == stm32::i2c_ref::Status::NACK)
+  {
+    success = false;
+  }
 
-    // switch config byte
-    uint8_t switch_configuration = (static_cast<uint8_t>(sw_throw) | static_cast<uint8_t>(sw_pole));
-    stm32::i2c::send_byte(m_i2c_handle, switch_configuration);
-    // latch byte
-    if (sw_latch == Latch::set)
-    {
-        stm32::i2c::send_byte(m_i2c_handle, 0x01);
-    }
-    else
-    {
-        stm32::i2c::send_byte(m_i2c_handle, 0x00);
-    }
-    
-    // Generate the stop condition
-	stm32::i2c::generate_stop_condition(m_i2c_handle);
-    return success;
+  // switch config byte
+  uint8_t switch_configuration = (static_cast<uint8_t>(sw_throw) | static_cast<uint8_t>(sw_pole));
+  stm32::i2c_ref::send_byte(m_i2c_handle, switch_configuration);
+  // latch byte
+  if (sw_latch == Latch::set)
+  {
+    stm32::i2c_ref::send_byte(m_i2c_handle, 0x01);
+  }
+  else
+  {
+    stm32::i2c_ref::send_byte(m_i2c_handle, 0x00);
+  }
+
+  // Generate the stop condition
+  stm32::i2c_ref::generate_stop_condition(m_i2c_handle);
+  return success;
 }
 
 // See page 22 of https://www.analog.com/media/en/technical-documentation/data-sheets/adg2188.pdf
 bool Driver::read_xline_switch_values(XLineRead line [[maybe_unused]])
 {
-    bool success {true};
+  bool success{true};
 
-    // write this number of bytes: The data byte(s) AND the address byte
-	stm32::i2c::set_numbytes(m_i2c_handle, 2);
+  // write this number of bytes: The data byte(s) AND the address byte
+  stm32::i2c_ref::set_numbytes(m_i2c_handle, 2);
 
-    // check ADG2188 is listening on 0xE0 + 1.
-	if (stm32::i2c::initialise_slave_device(m_i2c_handle, i2c_addr, stm32::i2c::StartType::WRITE) == stm32::i2c::Status::NACK) 
-    {
-        success = false;
-    }
-    // request the xline we want to read (second byte is don't care, so just repeat it)
-    stm32::i2c::send_byte(m_i2c_handle, static_cast<uint8_t>(line));
-    stm32::i2c::send_byte(m_i2c_handle, static_cast<uint8_t>(line));
-    
-    // Generate the stop condition
-	stm32::i2c::generate_stop_condition(m_i2c_handle);
+  // check ADG2188 is listening on 0xE0 + 1.
+  if (stm32::i2c_ref::initialise_slave_device(m_i2c_handle, i2c_addr, stm32::i2c_ref::StartType::WRITE) == stm32::i2c_ref::Status::NACK)
+  {
+    success = false;
+  }
+  // request the xline we want to read (second byte is don't care, so just repeat it)
+  stm32::i2c_ref::send_byte(m_i2c_handle, static_cast<uint8_t>(line));
+  stm32::i2c_ref::send_byte(m_i2c_handle, static_cast<uint8_t>(line));
 
-    // check ADG2188 is listening on 0xE0 + 0.
-	if (stm32::i2c::initialise_slave_device(m_i2c_handle, i2c_addr, stm32::i2c::StartType::READ) == stm32::i2c::Status::NACK) 
-    {
-        success = false;
-    }    
+  // Generate the stop condition
+  stm32::i2c_ref::generate_stop_condition(m_i2c_handle);
 
-    // receive the first byte and send back ACk to slave
-    uint8_t rx_byte1 {0};
-    stm32::i2c::receive_byte(m_i2c_handle, rx_byte1);
-    stm32::i2c::send_ack(m_i2c_handle);
+  // check ADG2188 is listening on 0xE0 + 0.
+  if (stm32::i2c_ref::initialise_slave_device(m_i2c_handle, i2c_addr, stm32::i2c_ref::StartType::READ) == stm32::i2c_ref::Status::NACK)
+  {
+    success = false;
+  }
 
-    // receive the second byte, send NACK and STOP to slave
-    uint8_t rx_byte2 {0};
-    stm32::i2c::receive_byte(m_i2c_handle, rx_byte2);
-    stm32::i2c::send_nack(m_i2c_handle);
-    
-	stm32::i2c::generate_stop_condition(m_i2c_handle);
-    return success;
+  // receive the first byte and send back ACk to slave
+  uint8_t rx_byte1{0};
+  stm32::i2c_ref::receive_byte(m_i2c_handle, rx_byte1);
+  stm32::i2c_ref::send_ack(m_i2c_handle);
+
+  // receive the second byte, send NACK and STOP to slave
+  uint8_t rx_byte2{0};
+  stm32::i2c_ref::receive_byte(m_i2c_handle, rx_byte2);
+  stm32::i2c_ref::send_nack(m_i2c_handle);
+
+  stm32::i2c_ref::generate_stop_condition(m_i2c_handle);
+  return success;
 }
 
 } // namespace adg2188
